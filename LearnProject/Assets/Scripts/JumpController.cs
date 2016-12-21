0bbbded978f8967206332040cb385c50515c9bcb
Brago <bragolab@gmail.com>
2016-12-21 01:02:04 +0100

    - Simple platform 2d.

---
+++ LearnProject/Assets/Scripts/JumpController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpController : MonoBehaviour {
+
+    public float JumpForce = 6;
+
+    bool isGrounded = false;
+    Rigidbody2D rb;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.J) && isGrounded == true) {
+            rb.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D coll) {
+        isGrounded = true;
+    }
+
+    void OnCollisionStay2D(Collision2D coll) {
+        isGrounded = true;
+    }
+
+    void OnCollisionExit2D(Collision2D coll) {
+        isGrounded = false;
+    }
+}
